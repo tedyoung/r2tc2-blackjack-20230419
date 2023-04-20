f238e15e72ec2ee0a4df3d1abf82a22ea8f01384
Ted M. Young <tedyoung@gmail.com>
2023-04-20 10:32:56 -0700

    LAB #3: Moved input to ConsoleGame and back of card display to ConsoleCard

--- src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
@@ -1,8 +1,7 @@
 package com.jitterted.ebp.blackjack.domain;
 
+import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleCard;
 import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleHand;
-
-import java.util.Scanner;
 
 import static org.fusesource.jansi.Ansi.ansi;
 
@@ -53,19 +52,13 @@ public class Game {
         }
     }
 
-    public String inputFromPlayer() {
-        System.out.println("[H]it or [S]tand?");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
     public void displayGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
         System.out.println(ConsoleHand.displayFaceUpCard(dealerHand));
 
         // second card is the hole card, which is hidden, or "face down"
-        displayBackOfCard();
+        ConsoleCard.displayBackOfCard();
 
         System.out.println();
         System.out.println("Player has: ");
@@ -83,20 +76,6 @@ public class Game {
         System.out.println("Player has: ");
         System.out.println(ConsoleHand.cardsAsString(playerHand));
         System.out.println(" (" + playerHand.value() + ")");
-    }
-
-    public void displayBackOfCard() {
-        System.out.print(
-                ansi()
-                        .cursorUp(7)
-                        .cursorRight(12)
-                        .a("┌─────────┐").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ J I T ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E R ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░ T E D ░│").cursorDown(1).cursorLeft(11)
-                        .a("│░░░░░░░░░│").cursorDown(1).cursorLeft(11)
-                        .a("└─────────┘"));
     }
 
     public void playerHits() {
